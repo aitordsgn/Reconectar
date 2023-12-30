edfd81c855921f6f35778f99285d119a2af25be7
aitordsgn <aitordsgn@gmail.com>
2023-12-30 19:49:24 +0100

    Mayor .css changes

--- src/GameCard.jsx
+++ src/GameCard.jsx
@@ -3,7 +3,7 @@ import "./GameCard.css"
 export function GameCard({imgName, gameName, tags}){
     return(
        <div className="card">
-            <img className="card-image" src={`src/assets/${imgName}`} alt="Splash screen"></img>
+            <img className="card-image" src={`src/assets/imgs/${imgName}`} alt="Splash screen"></img>
             <h2 className="card-title">{gameName}</h2>
             <p className="card-tag">{tags}</p>
        </div> 
