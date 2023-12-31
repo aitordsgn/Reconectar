a0faeea61c23f4f8fb11a90a28aaea9c2ec5bebd
aitordsgn <aitordsgn@gmail.com>
2023-12-31 15:10:14 +0100

    NameChange

--- src/GameList.jsx
+++ src/GameList.jsx
@@ -3,12 +3,16 @@ import Videogames from './Videogames.json'
 import Navbar from './navbar.jsx'
 
 
-export function App() {
+export function ListadoJuegos() {
     return(
         <>
         <Navbar/>
         <h1 className= 'gamePage-title' > Videojuegos </h1>
         <h4 className='gamePage-Desc'> Descubre los mejores juegos para jugar en pareja</h4>
+        <div>
+            <button className='button-tipo' type="button">Tipo</button>
+            <button className='button-fav' type="button">Favorito</button>
+        </div>
         <div className='gameGrid'>
             {
                 Videogames.map(videogame => {
