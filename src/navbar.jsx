b3f5a58f514297892b7a37ca467255b111c2a629
aitordsgn <aitordsgn@gmail.com>
2024-02-29 22:36:56 +0100

    Nav css modules

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,32 +1,72 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faTwitter, } from '@fortawesome/free-brands-svg-icons'
-import {Link} from 'react-router-dom'
-export default function Navbar(){
-    return(
-        <>
-        <header className="nav-head">
-            <a href="/" className="logo-container">
-                <img src="./Icon.svg" alt="Web Logo" className="logo"/>
-            </a>
-            <nav className= "nav">
-                <ul className="nav-ul">
-                    <li className="nav-li"><Link to="/">Ideas</Link></li>
-                    <li className="nav-li"><Link to="/Videojuegos">Videojuegos</Link></li>
-                    <li className="nav-li"><Link to="/Eventos">Eventos</Link></li>
-                </ul>
-            </nav>
-            <div className="social-buttons">
-                <a  href="https://twitter.com/reyortegaitor" target="_blank" rel="noopener noreferrer"aria-label="Twitter" className='nav-logos'>
-                    <FontAwesomeIcon icon={faTwitter} className="nav-logos"/>
-                </a>
-                <a  href="https://github.com/aitordsgn" target="_blank" rel="noopener noreferrer" aria-label="Github" className='nav-logos'>
-                    <FontAwesomeIcon icon={faGithub} className="nav-logos"/>
-                </a>
-                <a  href="https://github.com/aitordsgn" target="_blank" rel="noopener noreferrer" aria-label="Github " className='nav-donate'>
-                    Donate
-                </a>
-            </div>
-        </header>
-        </>
-    )
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
+
+// Define CSS modules styles using a separate file (e.g., Navbar.module.css)
+import styles from './nav.module.css';
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className={styles.navHead}>
+        <a
+          href="/"
+          className={styles.logoContainer}
+          rel="nofollow noopener"
+          aria-label="Web Logo"
+        >
+          <img src="./Icon.svg" alt="Web Logo" className={styles.logo} />
+        </a>
+        <nav className={styles.nav}>
+          <ul className={styles.navUl}>
+            <li className={styles.navLink}>
+              <Link to="/" className={styles.navLink}>
+                Ideas
+              </Link>
+            </li>
+            <li className={styles.navLink}>
+              <Link to="/Videojuegos" className={styles.navLink}>
+                Videojuegos
+              </Link>
+            </li>
+            <li className={styles.navLink}>
+              <Link to="/Eventos" className={styles.navLink}>
+                Eventos
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.socialButtons}>
+          <a
+            href="https://twitter.com/reyortegaitor"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Twitter"
+            className={styles.navLogos}
+          >
+            <FontAwesomeIcon icon={faTwitter} className={styles.icon} />
+          </a>
+          <a
+            href="https://github.com/aitordsgn"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Github"
+            className={styles.navLogos}
+          >
+            <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+          </a>
+          <a
+            href="https://github.com/aitordsgn"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Donate"
+            className={styles.navDonate}
+          >
+            Donate
+          </a>
+        </div>
+      </nav>
+    </>
+  );
 }
