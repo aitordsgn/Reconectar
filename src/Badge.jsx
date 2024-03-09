111cc8e0307c4146ba8f67338f8d2b9e0f915995
aitordsgn <aitordsgn@gmail.com>
2024-03-09 08:51:17 +0100

    Funcionamiento generador de ideas completo

--- src/Badge.jsx
+++ src/Badge.jsx
@@ -1,77 +1,73 @@
 import styled from 'styled-components';
 
 const CategoryButton = styled.button`
-    text-align: center;
-    text-decoration: none;
-    vertical-align: middle;
-    border-radius: 2vh;
-    padding: 10px 20px;
-    margin: 1vh 0.2vw;
-    font-family: 'Inter Variable' ,-apple-system,BlinkMacSystemFont,Roboto,"Helvetica Neue",sans-serif;
-    font-size: 1rem;
-    font-weight: normal;
-    width: fit-content;
-    height: fit-content;
-    border:1px;
-    transition: background-color 0.3s, color 0.3s;
-    background-color: ${(props => 
-        props.variant === "Cocina" 
-        ? "#FCECB0" 
-        : props.variant === "En Casa" 
-        ? "#DFF0FF" 
-        : props.variant === "Activo" 
-        ? "#FEDEBC"
-        : props.variant === "Fuera de Casa" 
-        ? "#BCFEFA" 
-        : props.variant === "Planificada" 
-        ? "#FEBCEC" 
-        : props.variant === "Especial" 
-        ? "#B8ADFF" 
-        : props.variant === "Naturaleza" 
-        ? "#C3F8DF" 
-        : props.variant === "DIY" 
-        ? "#FFC7C7" 
-        :"#D9D9D9")};
-    border-color: ${(props => 
-        props.variant === "Cocina" 
-        ? "#a19567" 
-        : props.variant === "En Casa" 
-        ? "#8daeca" 
-        : props.variant === "Activo" 
-        ? "#c0a68a"
-        : props.variant === "Fuera de Casa" 
-        ? "#85b8b4" 
-        : props.variant === "Planificada" 
-        ? "#aa759c" 
-        : props.variant === "Especial" 
-        ? "#7870ad" 
-        : props.variant === "Naturaleza" 
-        ? "#86b9a1" 
-        : props.variant === "DIY" 
-        ? "#c98d8d" 
-        :"#a5a4a4")};
-    color: black;
-    cursor: pointer;
-    
-    &:hover {
-        background-color: lightgray;
-        color: darkgray;
-        text-decoration: line-through;
-    }
-    &:selected {
-    background-color: #f0f0f0; /* Cambia el color de fondo */
-    color: #000; /* Cambia el color del texto */
-    border: 2px solid #000; /* Agrega un borde más grueso */
-    }
-
+   text-align: center;
+  text-decoration: none;
+  vertical-align: middle;
+  border-radius: 2vh;
+  padding: 10px 20px;
+  margin: 1vh 0.2vw;
+  font-family: 'Inter Variable', -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+  font-size: 1rem;
+  font-weight: normal;
+  width: fit-content;
+  height: fit-content;
+  border: 1px;
+  transition: all 0.3s ease-in-out;
+  background-color: ${(props) =>
+    props.selected === false
+      ? 'lightgray'
+      : props.variant === "Cocina"
+      ? "#FCECB0"
+      : props.variant === "En Casa"
+      ? "#DFF0FF"
+      : props.variant === "Activo"
+      ? "#FEDEBC"
+      : props.variant === "Fuera de Casa"
+      ? "#BCFEFA"
+      : props.variant === "Planificada"
+      ? "#FEBCEC"
+      : props.variant === "Especial"
+      ? "#B8ADFF"
+      : props.variant === "Naturaleza"
+      ? "#C3F8DF"
+      : props.variant === "DIY"
+      ? "#FFC7C7"
+      : "#D9D9D9"};
+  border-color: ${(props) =>
+    props.variant === "Cocina"
+      ? "#a19567"
+      : props.variant === "En Casa"
+      ? "#8daeca"
+      : props.variant === "Activo"
+      ? "#c0a68a"
+      : props.variant === "Fuera de Casa"
+      ? "#85b8b4"
+      : props.variant === "Planificada"
+      ? "#aa759c"
+      : props.variant === "Especial"
+      ? "#7870ad"
+      : props.variant === "Naturaleza"
+      ? "#86b9a1"
+      : props.variant === "DIY"
+      ? "#c98d8d"
+      : "#a5a4a4"};
+  color: ${(props) => (props.selected === false ? 'darkgray' : 'black')};
+  cursor: pointer;
+  text-decoration: ${(props) => (props.selected  === false ? 'line-through' : 'none')}; 
+  &:hover {
+    background-color: lightgray;
+    color: darkgray;
+    text-decoration:line-through;
+  }
     `;
     
-export function BadgeButton ({category}) {
-    return(
-        <>
-            <CategoryButton variant = {category}>
-                {category}
+    export function BadgeButton({ category, selected, onClick }) { // Agrega "selected" y "onClick"
+        return (
+          <>
+            <CategoryButton variant={category} selected={selected} onClick={onClick}>
+              {category}
             </CategoryButton>
-        </>
-    )
-}
+          </>
+        );
+      }
--- src/GeneradorIdeas.jsx
+++ src/GeneradorIdeas.jsx
@@ -1,53 +1,31 @@
-import { BadgeButton } from './Badge.jsx';
-import styles from './ideas.module.css';
-import { FaLightbulb, FaCalendar} from "react-icons/fa";
-
-import { useState } from 'react';
+// GeneradorIdeas.jsx
+import { useState, useEffect } from 'react';
 import ideasJson from './dates.json';
-
+import { FaLightbulb, FaCalendar } from "react-icons/fa";
+import { BadgeButton } from './Badge.jsx'; // Importar BadgeButton
+import styles from './ideas.module.css';
 
 function obtenerIdeaAleatoria(json, categoriasSeleccionadas) {
   // Filtrar las ideas según las categorías seleccionadas
   const ideasFiltradas = json.filter(idea =>
-    categoriasSeleccionadas.length === 0 || // Mostrar todas si no hay categorías seleccionadas
     categoriasSeleccionadas.includes(idea.Categoria)
   );
-
-  // Si no hay ideas coincidentes, mostrar un mensaje
+  console.log("Categorías Seleccionadas:", categoriasSeleccionadas);
+  console.log("Ideas Filtradas:", ideasFiltradas);
+  // Si no hay ideas coincidentes o no hay categorías seleccionadas, mostrar un mensaje
   if (ideasFiltradas.length === 0) {
+    console.log("No hay ideas para las categorías seleccionadas");
     return "No hay ideas para las categorías seleccionadas";
   }
 
   // Obtener una idea aleatoria de las ideas filtradas
   return ideasFiltradas[Math.floor(Math.random() * ideasFiltradas.length)];
 }
- const handleCategoryClick = (Cat) => {
-//   // Obtener el estado actual de las categorías
-//   const updatedCategoriasSeleccionadas = [...categoriasSeleccionadas];
 
-//   // Si la categoría ya está seleccionada, eliminarla
-//   if (updatedCategoriasSeleccionadas.includes(Cat)) {
-//     updatedCategoriasSeleccionadas.splice(updatedCategoriasSeleccionadas.indexOf(Cat), 1);
-//   } else {
-//     // Si no está seleccionada, agregarla
-//     updatedCategoriasSeleccionadas.push(Cat);
-//   }
-//   useEffect(() => {
-//     // Registrar el montaje del componente
-//     console.log('Componente Generador_Ideas montado');
-  
-//     // Registrar la desmontaje del componente
-//     return () => console.log('Componente Generador_Ideas desmontado');
-//   }, []);
-//   // Actualizar el estado con las categorías actualizadas
-//   setCategoriasSeleccionadas(updatedCategoriasSeleccionadas);
-//   console.log('Categorías:', categoriasSeleccionadas);
- };
 export function Generador_Ideas() {
   const [ideaActual, setIdeaActual] = useState('');
   const [categoriasSeleccionadas, setCategoriasSeleccionadas] = useState([]);
   const [mostrarTexto, setMostrarTexto] = useState(true);
-
 
   const categorias = [
     "En Casa",
@@ -57,43 +35,70 @@ export function Generador_Ideas() {
     "Especial",
     "DIY",
     "Cocina",
-    "Activo",
+    "Activo"
   ];
+
+  // Efecto para seleccionar todas las categorías si ninguna está seleccionada
+  useEffect(() => {
+    if (categoriasSeleccionadas.length === 0) {
+      setCategoriasSeleccionadas([  "En Casa",
+      "Fuera de Casa",
+      "Planificada",
+      "Naturaleza",
+      "Especial",
+      "DIY",
+      "Cocina",
+      "Activo"]); // Seleccionar por defecto las categorías "En Casa" y "Fuera de Casa"
+      console.log("Todas las categorías están seleccionadas");
+    }
+  }, [categoriasSeleccionadas, setCategoriasSeleccionadas]);
+
+  const handleCategoryClick = (categoria) => {
+    const updatedCategoriasSeleccionadas = [...categoriasSeleccionadas];
+    const categoriaIndex = updatedCategoriasSeleccionadas.indexOf(categoria);
+    if (categoriaIndex !== -1) {
+      updatedCategoriasSeleccionadas.splice(categoriaIndex, 1);
+    } else {
+      updatedCategoriasSeleccionadas.push(categoria);
+    }
+    setCategoriasSeleccionadas(updatedCategoriasSeleccionadas); // Actualizar el estado
+    console.log("Categorías Seleccionadas:", updatedCategoriasSeleccionadas);
+  };
 
   return (
     <>
       <div className='BackgroundContainer'/>
       <div className={styles.categoriesContainer}>
-          {/* Crear botones BadgeButton para cada categoría */}
-          {categorias.map((categoria, index) => (
-            <BadgeButton
-              key={index}
-              category={categoria}
-              selected={categoriasSeleccionadas.includes(categoria)}
-              onClick={() => handleCategoryClick(categoria)}
-            />
-          ))}
-        </div>
-        <div className={styles.containerIdeas}>
+        {/* Crear botones BadgeButton para cada categoría */}
+        {categorias.map((categoria, index) => (
+          <BadgeButton
+            key={index}
+            category={categoria}
+            selected={categoriasSeleccionadas.includes(categoria)}
+            onClick={() => handleCategoryClick(categoria)}
+          />
+        ))}
+      </div>
+      <div className={styles.containerIdeas}>
         <h1 className={styles.DateIdea}>{mostrarTexto ? "Presiona el botón para generar una idea nueva" : ideaActual}</h1>
-          <div className={styles.buttonContainer}>
-            <button
-              className={styles.generateIdeaButton}
-              onClick={() => {
-                const ideaAleatoria = obtenerIdeaAleatoria(ideasJson, categoriasSeleccionadas);
-                setIdeaActual(ideaAleatoria.Valor);
-                setMostrarTexto(false);
-              }}
-            >
-              <FaLightbulb class={styles.CalendarIcon} />
-              <span className={styles.lable}>Generar Idea</span>
-            </button>
-            <button className={styles.Calendario}>
-              <FaCalendar class={styles.CalendarIcon} />
-              <span className={styles.lable}>Añadir Evento</span>
-            </button>
-          </div>
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.generateIdeaButton}
+            onClick={() => {
+              const ideaAleatoria = obtenerIdeaAleatoria(ideasJson, categoriasSeleccionadas);
+              setIdeaActual(ideaAleatoria.Valor);
+              setMostrarTexto(false);
+            }}
+          >
+            <FaLightbulb className={styles.CalendarIcon} />
+            <span className={styles.lable}>Generar Idea</span>
+          </button>
+          <button className={styles.Calendario}>
+            <FaCalendar className={styles.CalendarIcon} />
+            <span className={styles.lable}>Añadir Evento</span>
+          </button>
         </div>
+      </div>
     </>
   );
 }
