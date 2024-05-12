b1e1a1e93b26de8a0cc2637a8aa3b22505a6d911
aitordsgn <aitordsgn@gmail.com>
2024-05-12 17:41:09 +0200

    Calendar styling

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,8 +3,6 @@ import styles from '../styles/Event.module.css'
 const Event = (evento) => {
     return (
         <div className={styles.notification}>
-            <div className={styles.notiglow}></div>
-            <div className={styles.notiborderglow}></div>
             <strong className={styles.notititle}>{evento.titulo}</strong>
             <p className={styles.notibody}>{evento.fecha}</p>
             <p className={styles.notibody}>{evento.tipo}</p>
--- src/components/ListadoEventos.jsx
+++ src/components/ListadoEventos.jsx
@@ -3,8 +3,8 @@ import Event from './Event'; // Importa el componente Event
 
 const ListadoEventos = ({ eventos }) => {
   return (
-    <div>
-      <h2>Próximos Eventos</h2>
+    <div className={styles.EventList}>
+      <h1>Próximas Citas</h1>
       <div className={styles.notificationContainer}> {/* Agrega un contenedor para los eventos */}
           {eventos.map((evento) => (
             <Event key={evento.id} {...evento}/>
